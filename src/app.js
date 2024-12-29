4bdcca83224e284c0b98f3922f6d9788e67f2375
Aarón Adrián Huayna Silva <adrianhsilva403@gmail.com>
2024-12-29 12:57:11 +0000

    commit de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let quien = [
+  "mi perro",
+  "mi abuela",
+  "mi hermana pequeña",
+  "mi vecino el loco"
+];
+let acto = [
+  "se comió una mosca muerta",
+  "vomitó",
+  "consiguió un record Guiness cazando palomas",
+  "mordió a un niño de 3 años"
+];
+let donde = [
+  "en el autobús",
+  "en la puerta de casa",
+  "en pleno colegio",
+  "en el mar del Caribe"
+];
+let cuando = [
+  "antes de clases.",
+  "mientras jugaba al minecraft",
+  "durante mi almuerzo",
+  "cuando estaba ligándome a mi gymsis"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generadorDeExcusas() {
+  return (
+    quien[Math.floor(Math.random() * quien.length)] +
+    " " +
+    acto[Math.floor(Math.random() * acto.length)] +
+    " " +
+    donde[Math.floor(Math.random() * donde.length)] +
+    " " +
+    cuando[Math.floor(Math.random() * cuando.length)]
+  );
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("excusa").innerHTML = generadorDeExcusas();
